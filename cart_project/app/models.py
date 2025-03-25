2f018240b099f96a8278a8e0ab1a01fe64fa00bc
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2025-03-24 23:07:46 -0500

    wip: model  setup

--- cart_project/app/models.py
+++ cart_project/app/models.py
@@ -28,4 +28,23 @@ class Order(models.Model):
         (COMPLETE,  'complete'),
         (SHIPPING,  'shipping')
     ]
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=ORDER_STATUS_CHOICES, default=PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
